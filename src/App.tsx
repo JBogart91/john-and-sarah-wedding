29f256da4dd216c0efb930063d9340454b0cc5a4
John Bogart <j.bogart91@gmail.com>
2023-11-05 21:20:29 +0000

    Add route example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from 'react'
+import { Link, Route } from "wouter";
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold">
-      John and Sarah 2024
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold">
+        John and Sarah 2024
+
+      </h1>
+      <Link href="/about">To About</Link>
+      <Route path="/about">
+        {() => <div>About</div>}
+      </Route>
+    </>
   )
 }
 
