b96de0b42946653ec6b510d503f399f227e76f5a
John Bogart <j.bogart91@gmail.com>
2023-11-05 21:07:33 +0000

    Install wouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
+    <h1 className="text-3xl font-bold">
+      John and Sarah 2024
     </h1>
   )
 }
