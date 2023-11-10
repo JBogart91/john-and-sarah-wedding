6297512b0834e615e913c20b9048487d124643a2
John Bogart <j.bogart91@gmail.com>
2023-11-10 21:40:55 +0000

    Install Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-function App() {
-
-  return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
-  )
-}
+const App = () => (
+  <>
+    <main>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
+    </main>
+  </>
+)
 
 export default App
