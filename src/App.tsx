b655d95ef16fa20abba2b83d0e1d37702ee41e2e
John Bogart <j.bogart91@gmail.com>
2023-11-12 18:38:31 +0000

    Install flowbite + set up basic nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { NavigationBar } from "./components/NavigationBar"
+
 const App = () => (
   <>
+    <nav>
+      <NavigationBar />
+    </nav>
     <main>
       <h1 className="text-3xl font-bold underline text-blue-500">
         Hello world!
