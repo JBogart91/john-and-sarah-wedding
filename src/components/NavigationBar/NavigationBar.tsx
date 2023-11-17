45299ab8da1c504f1ed4b1862b3f56a56c6cd08d
John Bogart <j.bogart91@gmail.com>
2023-11-17 18:28:41 +0000

    Fix toggling on navbar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,27 +1,43 @@
-import { Navbar } from 'flowbite-react'
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
-const NavigationBar = () => (
-  <Navbar fluid rounded>
-    <Navbar.Brand href="/">
-      <img src="../../favicon.ico" className="mr-3 h-6 sm:h-9 lg:hidden" alt="J&S Logo" />
-      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white lg:hidden">John & Sarah</span>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse>
-      <Navbar.Link href="/" className='lg:hidden'>
-        Home
-      </Navbar.Link>
-      <Navbar.Link href="/our-story">
-        Our Story
-      </Navbar.Link>
-      <Navbar.Link href="/rsvp">
-        RSVP
-      </Navbar.Link>
-      <Navbar.Link href="/contact">
-        Contact Us
-      </Navbar.Link>
-    </Navbar.Collapse>
-  </Navbar>
-)
+import { Navbar } from 'flowbite-react'
+import { Link } from 'wouter'
+
+const { useState } = React
+
+const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
+
+  const handleClick = () => {
+    setIsOpen(prevIsOpen => !prevIsOpen)
+  }
+
+  console.log('in Nav bar', isOpen)
+  return (
+    <Navbar>
+      <Navbar.Brand as={Link} href="/">
+        <img src="../../favicon.ico" className="mr-3 h-6 sm:h-9" alt="J&S Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle onClick={handleClick} />
+      <Navbar.Collapse className={isOpen ? '' : 'hidden'}>
+        <Navbar.Link as={Link} href="/" className='lg:hidden'>
+          Home
+        </Navbar.Link>
+        <Navbar.Link as={Link} href="/our-story" onClick={handleClick}>
+          Our Story
+        </Navbar.Link>
+        <Navbar.Link as={Link} href="/rsvp" onClick={handleClick}>
+          RSVP
+        </Navbar.Link>
+        <Navbar.Link as={Link} href="/contact" onClick={handleClick}>
+          Contact Us
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
 
 export default NavigationBar
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wedding-green': '#005421',
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
