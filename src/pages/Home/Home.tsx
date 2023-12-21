644cc311664ff09bc51f99804c79641090fbc1f9
John Bogart <j.bogart91@gmail.com>
2023-12-21 21:22:43 +0000

    Tweak to header

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,10 @@
 const Home = (): JSX.Element => {
   return (
     <div>
-      <h1 className="font-header text-8xl text-center">We&apos;re getting married!</h1>
+      <div>
+        <h1 className="font-header text-5xl sm:text-8xl text-center">John & Sarah</h1>
+        <h2 className="font-header text-3xl sm:text-7xl text-center">We&apos;re getting married!</h2>
+      </div>
     </div>
   )
 }
