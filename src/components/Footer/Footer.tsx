494670865c9985809cee65b2e12b761da8dbff18
John Bogart <j.bogart91@gmail.com>
2024-01-12 13:46:07 +0000

    Sticky footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = (): JSX.Element => (
+  <footer className="fixed left-0 bottom-0 w-full bg-white">
+    <div className="flex justify-center">
+        <div className="flex flex-col text-gray-500 w-20 text-center">
+        <span className="pb-0.5">J &  S</span>
+        <span className="border-t-solid border-t-gray-200 border-t">2.11.2024</span>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
