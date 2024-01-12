d1bb1602b371a5585378e88c09e2c6c644f80f94
John Bogart <j.bogart91@gmail.com>
2024-01-12 16:07:29 +0000

    Mobile wip

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,22 +2,28 @@ import { Link } from 'wouter'
 
 const Home = (): JSX.Element => {
   return (
-    <div className="grid">
-      <div className="mb-4 md:relative">
+    <div className="grid grid-row-4 gap-0">
+      <div className="row-span-1">
         <img src="../src/assets/splash(2).jpeg" />
-        <div className="hidden md:block md:absolute md:bg-white md:top-4 md:left-4">John & Sarah</div>
       </div>
-      <div className="grid grid-rows-2 gap-4 md:hidden">
-        <div className="grid grid-rows-2 divide-y divide-current">
-          <span className="text-center md:text-xl pb-1">Sandhole Oak Barn, Cheshire</span>
-          <span className="text-center md:text-xl">November 2nd 2024</span>
+      <div className="row-span-1 flex flex-col mt-4 text-center">
+        <div className="flex flex-col text-center">
+          <h1 className="font-header text-3xl mb-2">John & Sarah</h1>
+          <h2 className="font-header text-2xl mb-2">We&apos;re getting married!</h2>
+          <span className="pb-2 border-b-solid border-b-black border-b">Sandhole Oak Barn, Cheshire</span>
+          <span className="pt-2">November 2nd 2024</span>
         </div>
-        <Link href={'/rsvp'}>
-          <button className="bg-lime-900 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded-full w-full h-12">
-            RSVP
-          </button>
-        </Link>
+        <div>
+          <div className="mb-2">We&apos;d love for you to join us!</div>
+          <Link href={'/rsvp'}>
+            <button className="bg-lime-900 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded-full w-full h-12">
+              RSVP
+            </button>
+          </Link>
+        </div>
       </div>
+      <div className="row-span-1 mt-4">Schedule for the Day</div>
+      <div className="row-span-1 mt-4">Venue</div>
     </div>
   )
 }
