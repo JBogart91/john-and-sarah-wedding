f240c998dc3a14e1144b89172038a799ab3df6bc
John Bogart <j.bogart91@gmail.com>
2024-02-17 14:06:53 +0000

    Wip

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,12 @@ const Home = (): JSX.Element => {
   return (
     <div className="grid grid-row-4 gap-0">
       <div className="row-span-1">
-        <img src="../src/assets/splash(2).jpeg" />
+        <h1 className="font-header text-6xl text-center mb-2 hidden lg:block">John & Sarah</h1>
+        <img src="../src/assets/IMG_3932.jpeg" />
       </div>
       <div className="row-span-1 flex flex-col mt-4 text-center">
         <div className="flex flex-col text-center">
-          <h1 className="font-header text-3xl mb-2">John & Sarah</h1>
+          <h1 className="font-header text-3xl mb-2 lg:hidden">John & Sarah</h1>
           <h2 className="font-header text-2xl mb-4">We&apos;re getting married!</h2>
           <span className="pb-2 border-b-solid border-b-black border-b">Sandhole Oak Barn, Cheshire</span>
           <span className="pt-2">November 2nd 2024</span>
