37459e4b0a14f4db480b6bb25f228650ba7c2dd9
John Bogart <j.bogart91@gmail.com>
2024-06-22 23:10:57 +0100

    Tweaks to titles

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 const Contact = (): JSX.Element => (
   <main className="max-w-5xl m-auto border-t-0 p-4">
-    <h1 className="font-header text-6xl md:text-8xl text-center">Contact Us</h1>
+    <h1 className="font-header text-4xl text-center mb-6 md:text-6xl underline decoration-1 underline-offset-4">Contact Us</h1>
   </main>
 )
 
--- src/pages/FAQS/FAQS.tsx
+++ src/pages/FAQS/FAQS.tsx
@@ -21,7 +21,7 @@ const FAQS = (): JSX.Element => {
 
   return (
     <main className="max-w-5xl m-auto border-t-0 p-4">
-      <h1 className="font-header text-6xl text-center">FAQs</h1>
+      <h1 className="font-header text-4xl text-center mb-6 md:text-6xl underline decoration-1 underline-offset-4">FAQs</h1>
       <div className="mt-8">
         {accordions.map((accordion) => (
           <Accordion
