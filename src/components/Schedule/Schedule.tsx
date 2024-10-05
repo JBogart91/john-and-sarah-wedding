b5714005e9a6b86fb7d81c42c0bbd463c404ab3e
John Bogart <j.bogart91@gmail.com>
2024-10-05 14:17:48 +0100

    tweak schedule to what its going to be

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -57,6 +57,21 @@ const Schedule = (): JSX.Element => {
           </div>
       </div>
       <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+        <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-wedding-green text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <img
+              src={speeches}
+              alt="speeches"
+              className="w-6 h-6 object-contain"
+            />
+        </div>
+        <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+              <div className="flex items-center justify-between space-x-2 mb-1">
+                  <div className="font-bold text-slate-900">Speeches</div>
+                  <time className="font-caveat font-medium">4:00pm</time>
+              </div>
+          </div>
+      </div>
+      <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
           <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-wedding-green text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
             <img
               src={breakfast}
@@ -67,25 +82,11 @@ const Schedule = (): JSX.Element => {
           <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
               <div className="flex items-center justify-between space-x-2 mb-1">
                   <div className="font-bold text-slate-900">Wedding Breakfast</div>
-                  <time className="font-caveat font-medium">3:45pm</time>
+                  <time className="font-caveat font-medium">4:15pm</time>
               </div>
           </div>
       </div>
-      <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
-          <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-wedding-green text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
-            <img
-              src={speeches}
-              alt="speeches"
-              className="w-6 h-6 object-contain"
-            />
-          </div>
-          <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
-              <div className="flex items-center justify-between space-x-2 mb-1">
-                  <div className="font-bold text-slate-900">Speeches</div>
-                  <time className="font-caveat font-medium">6:00pm</time>
-              </div>
-          </div>
-      </div>
+
       <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
           <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-wedding-green text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
             <img
